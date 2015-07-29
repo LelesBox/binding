9eaaa4d39744a10f8f3eedbb6ed44d0072f879df
shorly <li_xiaoliang@foxmail.com>
2015-07-29 10:52:04 +0800

    update

--- index.js
+++ index.js
@@ -83,22 +83,67 @@ var binding = function (args) {
                     }
                     if (d.name == "_class") {
                         var express = d.value.split(",");
-                        var func;
-                        for (var j = 0, str; str = express[j++];) {
-                            var _class = str.substr(0, str.indexOf(":"));
-                            var exs = str.substring(str.indexOf(":") + 1);
-                            console.log(_class)
-                            console.log(exs);
-                            exs = "return " + exs;
-                            func = new Function(exs);
-                            if (func()) {
-
-                            }
-                        }
+                        this._class(element, express);
                     }
                 }
             }
         },
+        _class: function (element, express) {
+            //匹配一个变量
+            for (var j = 0, str; str = express[j++];) {
+                var _class = str.substr(0, str.indexOf(":"));
+                var exs = str.substring(str.indexOf(":") + 1);
+                //扫描表达式，查找args里的参数，并当做参数传入表达式
+                var ps = this.extractParam(exs);
+                if (ps.length > 0) {
+                    var pstr = ps.join(",");
+                    var func = new Function(pstr, "return " + exs);
+                    var aps = [];
+                    for (var k = 0, m; m = ps[k++];) {
+                        aps.push(args.data[m]);
+                    }
+                    var result = func.apply(null, aps);
+                    console.log(result);
+                    //存入binding.classFuncs
+                    if (!binding.classFuncs[pstr]) {
+                        binding.classFuncs[pstr] = [];
+                    }
+                    binding.classFuncs[pstr].push({_class: _class, _func: func});
+                    //    监控参数
+                    observe(args.data, ps, function (name, value, oldvale) {
+                        var pt = ps.join(",");
+                        var aps = [];
+                        for (var k = 0, m; m = ps[k++];) {
+                            aps.push(args.data[m]);
+                        }
+                        binding.classFuncs[pt].forEach(function (item) {
+                            var result = item.apply(null, aps);
+                            console.log("class 要改变啦" + result)
+                        })
+                    })
+                } else {
+                    //    无参数，可是你为什么要写一个无参数的表达式呢，JJ酸
+
+                }
+            }
+        },
+        //从表达式里抽取现有的参数
+        extractParam: function (express) {
+            var params = [];
+            for (var s in args.data) {
+                //获取非函数变量
+                if (args.data.hasOwnProperty(s) && !isFunction(args.data[s])) {
+                    //匹配一个变量
+                    var rstr = "(^|\\W)(" + s + "(?!'))(\\W|$)";
+                    var rgx = new RegExp(rstr);
+                    if (rgx.test(express)) {
+                        params.push(s);
+                    }
+                }
+            }
+            return params;
+        },
+        //监听字符串，主要在处理{{文本}}
         obserStr: function (node, matchs, args) {
             observe(args.data, matchs, function (name, value, oldvalue) {
                 node.data = node.data.replace(new RegExp(oldvalue, "g"), function (match, value, index, str) {
@@ -121,12 +166,15 @@ var binding = function (args) {
     return binding.binds[args.id];
 }
 binding.binds = {};
+binding.classFuncs = {};
 
 function startWith(target, str, ignorCase) {
     var start_str = target.substr(0, str.length);
     return ignorCase ? start_str.toLowerCase() === str.toLowerCase() : start_str === str;
 }
-
+function isFunction(obj) {
+    return Object.prototype.toString.call(obj) == '[object Function]';
+}
 
 var test = binding({
     id: "test",
@@ -160,13 +208,53 @@ var test1 = binding({
     },
     //使用bind把上下文文传递过去
     bClick: function (x) {
+        //this.kaka++;
         this.callbind("test", "aClick", [this.kaka++]);
-        this.callbind("test", "data", {count: this.kaka++});
+        //this.callbind("test", "data", {count: this.kaka++});
     },
     init: function () {
-        this.kaka = 10086;
+        //this.kaka = 10086;
     }
 })
 var tpl = "return 2>1";
 var myFunction = new Function("a", "b", tpl);
 console.log(myFunction(2, 3))
+
+var s = {s: 1, b: 2}
+observe(s, "s", function () {
+    console.log("ssss")
+    console.log(arguments)
+})
+observe(s, ["s", "b"], function () {
+    console.log("bbbb")
+    console.log(arguments)
+})
+s.s = 2;
+s.b = 3;
+
+//var s = "var x=kaka.obj+1=2?'kaka':asd"
+////匹配一个变量
+//var reg = /\W(kaka(?!'))\W/g;
+//while (x = reg.exec(s)) {
+//    console.log(x)
+//}
+//
+//var reg = /\b\w+(?=ing\b)/g
+//var str = "I'm singing while you're dancing."
+//while (x = reg.exec(str)) {
+//    console.log(x)
+//}
+//var x = "kaka";
+//var regx = "\\W(" + x + "(?!'))\\W";
+//var rg = new RegExp(regx);
+//var r = /\W(kaka(?!'))\W/;
+//var g = /(^|\W)(kaka(?!'))(\W|$)/
+//var rstr = "(^|\\W)(kaka(?!'))(\\W|$)";
+//g = new RegExp(rstr);
+//console.log(g)
+//console.log(g.test("kaka+1==3"))
+
+//var s = {a: 2, b: 3, c: 4}
+//observe(s, ["a"], function (name, value, old) {
+//    s.a = 3;
+//})
